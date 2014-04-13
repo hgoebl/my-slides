504e06e690e9bb46e8fa88e9fccb71a064e21c00
Heinrich Goebl <hgoebl@goebl.com>
2014-04-13 19:59:27 +0200

    upgrade to nerdshow@0.3.0

--- generate-local.sh
+++ generate-local.sh
@@ -14,17 +14,17 @@ nerdshow-generate slides/mongodb/${PRESENTATION}    ${OPTIONS}
 echo "<html><head><title>Slides</title></head><body>" > ${INDEX}
 
 echo "<a href='javascript/'>JavaScript</a>" >> ${INDEX}
-echo "<a href='/nerdshow/rc/remote.html?/javascript'><img src='qrcode-javascript.png'/></a>" >> ${INDEX}
+echo "<a href='/nerdshow/rc/?/javascript'><img src='qrcode-javascript.png'/></a>" >> ${INDEX}
 
 echo "<br><br><br><br>" >> ${INDEX}
 
 echo "<a href='nodejs/'>Node.js</a>" >> ${INDEX}
-echo "<a href='/nerdshow/rc/remote.html?/nodejs'><img src='qrcode-nodejs.png'/></a>" >> ${INDEX}
+echo "<a href='/nerdshow/rc/?/nodejs'><img src='qrcode-nodejs.png'/></a>" >> ${INDEX}
 
 echo "<br><br><br><br>" >> ${INDEX}
 
 echo "<a href='mongodb/'>MongoDB</a>" >> ${INDEX}
-echo "<a href='/nerdshow/rc/remote.html?/mongodb'><img src='qrcode-mongodb.png'/></a>" >> ${INDEX}
+echo "<a href='/nerdshow/rc/?/mongodb'><img src='qrcode-mongodb.png'/></a>" >> ${INDEX}
 
 echo "</body></html>" >> ${INDEX}
 
